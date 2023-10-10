9862286aba06c0a206e8f1434e10da60c2bdb862
qiaopengjun <qiaopengjun0@gmail.com>
2023-10-10 16:19:00 +0800

    客户端按需加载element-plus前端组件

--- fuguangweb/src/router/index.ts
+++ fuguangweb/src/router/index.ts
@@ -2,12 +2,31 @@ import {createRouter, createWebHistory, Router, RouteRecordRaw} from "vue-router
 
 // 路由列表
 // RouterRecordRaw是路由组件对象
-const routes: RouteRecordRaw[] = []
+const routes: RouteRecordRaw[] = [
+    {
+        meta: {
+            title: "浮光在线教育-首页",
+            keepAlive: true
+        },
+        path: '/',         // uri访问地址
+        name: "Home",
+        component: () => import("../views/Home.vue")
+    },
+    {
+        meta: {
+            title: "浮光在线教育-用户登录",
+            keepAlive: true
+        },
+        path: '/login',      // uri访问地址
+        name: "Login",
+        component: () => import("../views/Login.vue")
+    }
+]
 
 // 路由对象实例化
 const router: Router = createRouter({
     // history, 指定路由的模式
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     // 路由列表
     routes,
 });
--- fuguangweb/vite.config.ts
+++ fuguangweb/vite.config.ts
@@ -1,7 +1,59 @@
-import { defineConfig } from 'vite'
+import path from 'path'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import Inspect from 'vite-plugin-inspect' // https://github.com/antfu/vite-plugin-inspect
+
+const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': pathSrc,
+        },
+    },
+    plugins: [
+        vue(),
+        AutoImport({
+            // global imports to register
+            imports: [
+                // presets
+                'vue',
+                'vue-router'
+            ], // 自动导入 vue 相关函数，如 ref reactive toRef 等
+            resolvers: [
+                ElementPlusResolver(),
+
+                // Auto import icon components
+                // 自动导入图标组件
+                IconsResolver({
+                    // prefix: 'Icon',
+                })
+            ],
+            eslintrc: {enabled: true}, // eslint 会报 no-undef 错误，添加此项会生成 .eslintrc-auto-import.json
+            // 生成自动导入的TS声明文件
+            dts: path.resolve(pathSrc, 'auto-imports.d.ts'),
+        }),
+        Components({
+            resolvers: [
+                // Auto register icon components
+                // 自动注册图标组件
+                IconsResolver({
+                    enabledCollections: ['ep'],
+                }),
+                ElementPlusResolver()
+            ], // 自动导入 Element Plus 组件，图标组件
+            dts: path.resolve(pathSrc, 'components.d.ts'),
+        }),
+        Icons({
+            autoInstall: true,
+        }),
+
+        Inspect(),
+    ],
 })
