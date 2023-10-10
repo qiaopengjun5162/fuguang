caa88f5010fb308d5997b3b9ae8d5055225dac9e
qiaopengjun <qiaopengjun0@gmail.com>
2023-10-10 17:53:47 +0800

    客户端显示首页

--- fuguangweb/src/components.d.ts
+++ fuguangweb/src/components.d.ts
@@ -10,6 +10,8 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElRate: typeof import('element-plus/es')['ElRate']
     ElRow: typeof import('element-plus/es')['ElRow']
+    Footer: typeof import('./components/Footer.vue')['default']
+    Header: typeof import('./components/Header.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
